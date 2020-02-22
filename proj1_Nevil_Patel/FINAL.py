94a2e860d4434f6bc76ef2fd61a221c3eecc75c9
nevil-patel7 <56212791+nevil-patel7@users.noreply.github.com>
2020-02-22 00:23:37 -0500

    Add files via upload

--- proj1_Nevil_Patel/FINAL.py
+++ proj1_Nevil_Patel/FINAL.py
@@ -25,11 +25,26 @@ Merged_Generate_Path = []
 Merged_Nodes_Info = []
 x=0
 
+#To check the solvability of matrix
+def Check_Puzzle(Matrix):
+    inv_count = 1
+    for i in range(0,8):
+        for j in range(i+1,9):
+            if Matrix[i] > Matrix[j] and Matrix[i] != 0:
+                inv_count = inv_count + 1
+
+    if inv_count%2 == 0:
+        print("PUZZLE IS SOLVABLE")
+        return True
+    else:
+        print("PUZZLE IS NOT SOLVABLE")
+        return False
+
 #Takes User input of Puzzle Matrix
 def Start_Position():
     TempNode = []
     InitialNode = []
-    TempNode = list(map(int, input("Enter the row-wise Initial Configuration separated by spaces\n").strip().split()))[:9]
+    TempNode = list(map(int, input("Enter the Row-wise Initial Configuration separated by spaces\n").strip().split()))[:9]
     if len(TempNode) is not 9:
         GetInitialNode()
     InitialNode.append(TempNode[:3])
@@ -152,96 +167,115 @@ def print_matrix(state):
         counter = counter +1
         print("\n-------------")
 
+Merged_Nodes_S=[]
+New_S_Node =[]
 #Doing the Initial Configuration
 StartNode = Start_Position()
-CurrentNode = copy.deepcopy(StartNode)
-Node_stat.append(StartNode)
-Parent_Node_Index_i.append(0)
-Node_Index.append(0)
+print(StartNode)
+New_S_Node = copy.deepcopy(StartNode)
+print(New_S_Node)
+reemovNestings(New_S_Node,Merged_Nodes_S)
+print(Merged_Nodes_S)
+Check = Check_Puzzle(Merged_Nodes_S)
 
-#Solving the puzzle by moving zero in all Four directions and by updating the List with Node infos
-while (Goal_Node != CurrentNode):
-    if (ActionMoveLeft(CurrentNode)[1]) != None:
-        [Status,NewNode] = ActionMoveLeft(CurrentNode)
-        Same_Status = AddNode(NewNode)
-        if Same_Status == False:
-            x=x+1
-            Node_Index.append(x)
+if Check == True:
+    CurrentNode = copy.deepcopy(StartNode)
+    Node_stat.append(StartNode)
+    Parent_Node_Index_i.append(0)
+    Node_Index.append(0)
+
+    #Solving the puzzle by moving zero in all Four directions and by updating the List with Node infos
+    while (Goal_Node != CurrentNode):
+        if (ActionMoveLeft(CurrentNode)[1]) != None:
+            [Status,NewNode] = ActionMoveLeft(CurrentNode)
+            Same_Status = AddNode(NewNode)
+            if Same_Status == False:
+                x=x+1
+                Node_Index.append(x)
 
 
-    if (ActionMoveRight(CurrentNode)[1]) != None:
-        [Status,NewNode] = ActionMoveRight(CurrentNode)
-        Same_Status = AddNode(NewNode)
-        if Same_Status == False:
-            x=x+1
-            Node_Index.append(x)
+        if (ActionMoveRight(CurrentNode)[1]) != None:
+            [Status,NewNode] = ActionMoveRight(CurrentNode)
+            Same_Status = AddNode(NewNode)
+            if Same_Status == False:
+                x=x+1
+                Node_Index.append(x)
 
-    if (ActionMoveUp(CurrentNode)[1]) != None:
-        [Status,NewNode] = ActionMoveUp(CurrentNode)
-        Same_Status = AddNode(NewNode)
-        if Same_Status == False:
-            x=x+1
-            Node_Index.append(x)
+        if (ActionMoveUp(CurrentNode)[1]) != None:
+            [Status,NewNode] = ActionMoveUp(CurrentNode)
+            Same_Status = AddNode(NewNode)
+            if Same_Status == False:
+                x=x+1
+                Node_Index.append(x)
 
-    if (ActionMoveDown(CurrentNode)[1]) != None:
-        [Status,NewNode] = ActionMoveDown(CurrentNode)
-        Same_Status = AddNode(NewNode)
-        if Same_Status == False:
-            x=x+1
-            Node_Index.append(x)
+        if (ActionMoveDown(CurrentNode)[1]) != None:
+            [Status,NewNode] = ActionMoveDown(CurrentNode)
+            Same_Status = AddNode(NewNode)
+            if Same_Status == False:
+                x=x+1
+                Node_Index.append(x)
 
-    New_Node_stat = Node_stat.index(CurrentNode)+1
-    CurrentNode = Node_stat[New_Node_stat]
-    Nodes_Info.append([Node_stat.index(CurrentNode), Parent_Node_Index_i[Node_stat.index(CurrentNode)],0])
-    print(Node_stat.index(CurrentNode),"CurrentNode",CurrentNode)
+        New_Node_stat = Node_stat.index(CurrentNode)+1
+        CurrentNode = Node_stat[New_Node_stat]
+        Nodes_Info.append([Node_stat.index(CurrentNode), Parent_Node_Index_i[Node_stat.index(CurrentNode)],0])
+        print(Node_stat.index(CurrentNode),"CurrentNode",CurrentNode)
 
-#Backtracking the Genrated Path
-Track_Path(CurrentNode)
-Get_Path(Back_Track)
-print("NodeIndex--> ",Node_Index)
-print("ParentNodeIndex-->",Parent_Node_Index_i)
-print("BranchToGoal-->",Back_Track)
-reemovNestings(Node_stat,Merged_Node_Stat)
-reemovNestings(Generate_Path,Merged_Generate_Path)
-reemovNestings(Nodes_Info,Merged_Nodes_Info)
+    #Backtracking the Genrated Path
+    Track_Path(CurrentNode)
+    Get_Path(Back_Track)
+    print("NodeIndex--> ",Node_Index)
+    print("ParentNodeIndex-->",Parent_Node_Index_i)
+    print("BranchToGoal-->",Back_Track)
+    reemovNestings(Node_stat,Merged_Node_Stat)
+    reemovNestings(Generate_Path,Merged_Generate_Path)
+    reemovNestings(Nodes_Info,Merged_Nodes_Info)
 
-#Creating the Text Files of explored Nodes and its path from Goal to start
+    #Creating the Text Files of explored Nodes and its path from Goal to start
 
-with open('Nodes.txt', 'w') as fh:
-        for x in range(0,len(Merged_Node_Stat),9):
-            for i in range(0,9):
-                fh.write(str(Merged_Node_Stat[x]) + ' ')
-                x = x+1
-            fh.write("\n")
-            
-with open('NodesInfo.txt', 'w') as fh:
-        for x in range(0,len(Merged_Nodes_Info),3):
-            fh.write(str(Merged_Nodes_Info[x]) + ' ')
-            fh.write(str(Merged_Nodes_Info[x+1]) + ' ')
-            fh.write(str(Merged_Nodes_Info[x+2]) + ' ')
-            fh.write("\n")
+    with open('Nodes.txt', 'w') as fh:
+            for x in range(0,len(Merged_Node_Stat),9):
+                for i in range(0,9):
+                    fh.write(str(Merged_Node_Stat[x]) + ' ')
+                    x = x+1
+                fh.write("\n")
 
-with open('nodePath.txt', 'w') as fh:
-        for x in range(0,len(Merged_Generate_Path),9):
-            for i in range(0,9):
+    with open('NodesInfo.txt', 'w') as fh:
+            for x in range(0,len(Merged_Nodes_Info),3):
+                fh.write(str(Merged_Nodes_Info[x]) + ' ')
+                fh.write(str(Merged_Nodes_Info[x+1]) + ' ')
+                fh.write(str(Merged_Nodes_Info[x+2]) + ' ')
+                fh.write("\n")
+
+    with open('nodePath.txt', 'w') as fh:
+            for x in range(0,len(Merged_Generate_Path),9):
                 fh.write(str(Merged_Generate_Path[x]) + ' ')
-                x = x+1
-            fh.write("\n")
+                fh.write(str(Merged_Generate_Path[x+3]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+6]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+1]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+4]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+7]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+2]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+5]) + ' ')
+                fh.write(str(Merged_Generate_Path[x+8]) + ' ')
+                fh.write("\n")
 
-#Printiing Path from Start to Goal Position
-print("PRINTING THE PATH")
-fname = 'nodePath.txt'
-data = np.loadtxt(fname)
-if len(data[1]) != 9:
-    print("Format of the text file is incorrect, retry ")
+    #Printiing Path from Start to Goal Position
+    print("PRINTING THE PATH")
+    fname = 'nodePath.txt'
+    data = np.loadtxt(fname)
+    if len(data[1]) != 9:
+        print("Format of the text file is incorrect, retry ")
+    else:
+        for i in range(0, len(data)):
+            if i == 0:
+                print("Start Node")
+            elif i == len(data)-1:
+                print("Achieved Goal Node")
+            else:
+                print("Step ",i)
+            print_matrix(data[i])
+            print()
+            print()
+
 else:
-    for i in range(0, len(data)):
-        if i == 0:
-            print("Start Node")
-        elif i == len(data)-1:
-            print("Achieved Goal Node")
-        else:
-            print("Step ",i)
-        print_matrix(data[i])
-        print()
-        print()
+    print("Exited The Code")
---
+++ proj1_Nevil_Patel/README.md
@@ -0,0 +1,12 @@
+# ENPM661-PROJECT1
+
+Files to solve 8 Puzzle Problem
+
+Libraries used in this code are Copy and numpy
+
+Run the Final.py file.
+Input the StartNod i.e. 3*3 Matrix(0...8)
+Genrated files shows:- 
+1)Node Informaton
+2)All the Nodes Discovered
+3)Path to Node from Start to End
